7a1e9785286f332f14cf0777d837f2316fdbeba0
TCherylene <cherylene@cartadatatek.com>
2026-09-05 16:50:27 +0700

    Databases: add migration for table_a - table_d

---
+++ database/migrations/2026_09_05_075439_create_table_a.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_a', function (Blueprint $table) {
+            $table->unsignedBigInteger('kode_toko_baru')->primary();
+            $table->integer('kode_toko_lama')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_a');
+    }
+};
